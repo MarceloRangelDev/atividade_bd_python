4678e41a6c8decdd9b22e17fcfc1821d6ef23c8b
MarceloRangelDev <71786320+MarceloRangelDev@users.noreply.github.com>
2025-11-10 19:40:37 -0300

    Cria a tabela livros

--- livros_sqlite.py
+++ livros_sqlite.py
@@ -3,3 +3,14 @@ import sqlite3
 # Cria o banco de dados livraria.db
 conn = sqlite3.connect('livraria.db')
 cursor = conn.cursor()
+
+# Verifica se a tabela livros existe, caso não exista ela será criada
+cursor.execute('''CREATE TABLE IF NOT EXISTS livros (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL UNIQUE,
+    autor TEXT,
+    ano INTEGER,
+    genero TEXT,
+    disponivel INTEGER CHECK(disponivel IN (0, 1))
+)''')
+
