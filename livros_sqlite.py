3b6a337a67286a78daf88cd68b50dfbfbaf4c983
MarceloRangelDev <71786320+MarceloRangelDev@users.noreply.github.com>
2025-11-10 19:46:31 -0300

    Cria 5 registros na tabela livros

--- livros_sqlite.py
+++ livros_sqlite.py
@@ -14,3 +14,15 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS livros (
     disponivel INTEGER CHECK(disponivel IN (0, 1))
 )''')
 
+# Inserir 5 Livros Fictícios
+# Adicione pelo menos 5 registros de livros na tabela criada.
+cursor.execute('''INSERT INTO livros (titulo, autor, ano, genero, disponivel) VALUES
+('O Senhor dos Anéis', 'J.R.R. Tolkien', 1954, 'Fantasia', 1),
+('O Hobbit', 'J.R.R. Tolkien', 1937, 'Fantasia', 0),
+('A Guerra dos Tronos', 'George R.R. Martin', 1996, 'Fantasia', 1),
+('O Código Da Vinci', 'Dan Brown', 2003, 'Suspense', 0),
+('Harry Potter e a Pedra Filosofal', 'J.K. Rowling', 1997, 'Fantasia', 1)
+''')
+
+conn.commit()
+conn.close()
